ef96597b0c73929f10a421b3aa98cd301702d16b
chris <armoredmoose@gmail.com>
2023-01-03 10:06:01 -0600

    Added comments

--- Utils.lua
+++ Utils.lua
@@ -11,6 +11,12 @@ Utils.__index = Utils
 _ENV = Utils
 
 
+--fill in the given table with the given val.
+--val can be either a single valule or a callback function.
+--stop is the index to stop in. default is the lenght of table.
+--start is index to start at. default is 1.
+--incr is the increment value for each iteration. default is 1.
+--returns table after filling it in.
 function Utils:fillTbl(tbl,val,stop,start,incr)
     local startI <const> = start and start or 1
     local stopI <const> = stop and stop or #tbl
@@ -20,6 +26,8 @@ function Utils:fillTbl(tbl,val,stop,start,incr)
     return self:genericILoop(tbl,startI,stopI,tblIncr,func)
 end
 
+
+--return a copy of the given table.
 function Utils:copyTbl(tbl)
     local tblCpy <const> = {}
     for i=1,#tbl,1 do
@@ -28,11 +36,14 @@ function Utils:copyTbl(tbl)
     return tblCpy
 end
 
+--return a table which is a shuffled version of the provided table.
 function Utils:randShuffleNewTbl(tbl)
     local newTbl <const> = self:copyTbl(tbl)
     return self:randShuffle(newTbl)
 end
 
+
+--shuffle a table in place. returns same table after it has been shuffled.
 function Utils:randShuffle(tbl) 
     self:genericILoop(tbl,#tbl,1,-1,
         function(index,value) 
@@ -44,6 +55,7 @@ function Utils:randShuffle(tbl)
     end)
     return tbl
 end
+
 
 function Utils:genericIJLoop(values,startI,stopI,incI,startJ,stopJ,incJ,iFunc,jFunc)
     for i=startI,stopI,incI do

e5ba938225bdaf95dde1adfe4444c79bb8c3f43b
chris <armoredmoose@gmail.com>
2023-01-03 17:17:03 -0600

    fixed up string utils a bit

--- StringLib.lua
+++ StringLib.lua
@@ -1,19 +1,21 @@
-local match  = string.match
-local gsub   = string.gsub
-local rand   = math.random
-local type   = type
-local rep    = string.rep
-local unpack = table.unpack
-local pairs  = pairs
+local utils <const> = require('Utils')
 
-local StringLib  = {}
+local match  <const> = string.match
+local gsub   <const> = string.gsub
+local rand   <const> = math.random
+local type   <const> = type
+local rep    <const> = string.rep
+local unpack <const> = table.unpack
+local pairs  <const> = pairs
+
+local StringLib <const> = {}
 StringLib.__index = StringLib
 
 --pattern of a word
-local wordsMatch = "[^_,.;:\"\'?!%s\n\r/]+"
+local wordsMatch <const> = "[^_,.;:\"\'?!%s\n\r/]+"
 
 --pattern to match word surrounded by punctuation or space characters.
-local wordPat	= "[_.,:;\"\'?!%s/]*(" .. wordsMatch .. ")[/_.,:;\"\'?!%s\n\r]+"
+local wordPat <const> = "[_.,:;\"\'?!%s/]*(" .. wordsMatch .. ")[/_.,:;\"\'?!%s\n\r]+"
 
 _ENV = StringLib
 
@@ -22,30 +24,20 @@ _ENV = StringLib
 --if param is a string it is first converted to a table of words.
 --each time it is called it grabs a word at random from the table, so repeated values are expected.
 function StringLib:getRandom(str)
-	local words = (type(str) == "string" and StringLib:getWordsTbl(str)) or (type(str) == "table" and str)
+	local words <const> = (type(str) == "string" and StringLib:getWordsTbl(str)) or (type(str) == "table" and str)
 	return function()
 		return words[rand(1,#words)]
 	end
 end
 
---get a table which has been randomly shuffled.
-local function randShuffle(words)
-	for i= #words,1,-1 do
-        local temp <const> = words[i]
-        local newI <const> = rand(#words)
-        words[i] = words[newI]
-        words[newI] = temp
-	end
-	return shuff
-end
 
 --get a unique random word from a string or a table of words
 --if param is string then first convert it to a table of words.
 --if rollOver is true then once all the words in the original table of words have been used, the word table is reshuffled and is used again.
 --if rollOver is false or nil then after all words have been used up all subsequent calls will return an empty string.
 function StringLib:getUniqueRandom(str,rollOver)
-	local words   = (type(str) == "string" and StringLib:getWordsTbl(str)) or (type(str) == "table" and str)
-	local shuffle = randShuffle(words)
+	local words <const> = (type(str) == "string" and StringLib:getWordsTbl(str)) or (type(str) == "table" and utils:copyTbl(str) )
+	local shuffle = utils:randShuffle(words)
 	return function()
 		if #shuffle == 0 then
 			if rollOver then
@@ -54,7 +46,7 @@ function StringLib:getUniqueRandom(str,rollOver)
 				return ""
 			end
 		end
-		local word = shuffle[#shuffle]
+		local word <const> = shuffle[#shuffle]
 		shuffle[#shuffle] = nil
 		return word
 	end
@@ -68,13 +60,13 @@ end
 
 --grab the nth word from the string.
 function StringLib:grabWord(str,n)
-	local pat = "^%s*" .. rep("[%w%-]+[%s%p]*",n - 1) .. "([%w%-]+)"
+	local pat <const> = "^%s*" .. rep("[%w%-]+[%s%p]*",n - 1) .. "([%w%-]+)"
 	return match(str,pat)
 end
 
 --get the character at position i.
 function StringLib:charAt(str,n)
-    local pat = "^" .. rep(".",n - 1) .. "(.)"
+    local pat <const> = "^" .. rep(".",n - 1) .. "(.)"
     return match(str,pat)
 end
 
@@ -112,9 +104,9 @@ end
 --use patter to set each index or if nil then each index is one character from the string.
 --if sep isnt nil then append it to the end of each index in table..
 function StringLib:strToTbl(str,pat,sep)
-	local t = {}
-	local pattern = pat and pat or "."
-	local add = sep and function(char) t[#t + 1] = char .. sep end or function(char) t[#t + 1] = char end
+	local t <const> = {}
+	local pattern <const> = pat and pat or "."
+	local add <const> = sep and function(char) t[#t + 1] = char .. sep end or function(char) t[#t + 1] = char end
 	for char in str:gmatch(pattern) do
 		add(char)
 	end
@@ -138,33 +130,33 @@ end
 
 --split string at the nth word. if include is true then first string includes split word.
 function StringLib:splitAround(str,n,include)
-    local i = include and n or n -1
+    local i <const> = include and n or n -1
     return split(str,"[%s%p]*[%w%-]+[%s%p]*",i)
 end
 
 --split string at first instance of the word. if include is true then split string contains word.
 function StringLib:splitAtWord(str,word,include)
-    local pat = include and "(.-" .. word .. ")(.*)" or "(.-)" .. word .. "(.*)"
+    local pat <const> = include and "(.-" .. word .. ")(.*)" or "(.-)" .. word .. "(.*)"
     return match(str,pat)
 end
 
 --split string on each occurrence of the word.
 function StringLib:splitByWord(str,word,whence,include)
-    local pat = (whence == "after" and include and "(.-".. word .. ")%s*" or "(.-)".. word .. "%s*") or
+    local pat <const> = (whence == "after" and include and "(.-".. word .. ")%s*" or "(.-)".. word .. "%s*") or
             (whence == "before" and include and "(.-)".. word .. "%s*")
-    local tbl = self:strToTbl(str,pat)
+    local tbl <const> = self:strToTbl(str,pat)
     tbl[#tbl + 1] = match(str,"^.+"..word.."%s*(.-)$")
     return unpack(tbl)
 end
 
 --place a % in front of special regex chars.
 local function sanitize(str)
-	local str1 = str:gsub("([+%-%%%(%)%[%]%*%.%^%$])","%%%1")
+	local str1 <const> = str:gsub("([+%-%%%(%)%[%]%*%.%^%$])","%%%1")
 	return str1
 
 end
 
---format str by using substituting values form the key/values of the given table.
+--format str by using substituting values from the key/values of the given table.
 function StringLib:format(str,formatters)
 	for search,sub in pairs(formatters)do
 		str = str:gsub("%${?"..sanitize(search).."}?",sub)
@@ -191,13 +183,13 @@ end
 
 -- iterate over each character in str.
 function StringLib:charIter(str)
-	local tbl = self:strToTbl(str)
+	local tbl <const> = self:strToTbl(str)
 	return  tableIter,tbl,0
 end
 
 --iterate over each word in str.
 function StringLib:wordIter(str)
-	local tbl = self:strToTbl(str,"(%w*%p*)%s*")
+	local tbl <const> = self:strToTbl(str,"(%w*%p*)%s*")
 	return  tableIter,tbl,0
 end
 
